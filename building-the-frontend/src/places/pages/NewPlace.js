9336fc11aa784491da39bd85cd171c12e818a4e2
Dan B <dbroughton_09@hotmail.com>
2020-05-27 23:25:53 +0100

    add more course videos complete

--- building-the-frontend/src/places/pages/NewPlace.js
+++ building-the-frontend/src/places/pages/NewPlace.js
@@ -7,7 +7,14 @@ const NewPlace = () => {
     return (
         <form className="place-form">
             {/* <Input type="text" label="Title" validators={[]} onChange={}/> */}
-            <Input element="input" type="text" label="Title" placeholder="Please enter your name..."/>
+            <Input 
+                element="input" 
+                type="text" 
+                label="Title" 
+                placeholder="Please enter your name..."
+                validators={[]}
+                errorText="Please enter a valid title."
+            />
         </form>
     )
 }
